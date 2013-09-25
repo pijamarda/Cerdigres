a81762483c8e29c070de8cc51163cf817137b76c
Zeneke <zeneke@gmail.com>
2013-09-25 16:08:10 +0200

    actualizacion desde el PC del curro

--- Cerdigres-desktop/src/com/zeneke/cerdigres/Main.java
+++ Cerdigres-desktop/src/com/zeneke/cerdigres/Main.java
@@ -9,8 +9,8 @@ public class Main {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "Cerdigres";
 		cfg.useGL20 = true;
-		cfg.width = 800;
-		cfg.height = 480;
+		cfg.width = 480;
+		cfg.height = 600;
 
 		new LwjglApplication(new MainCerdigres(), cfg);
 	}
